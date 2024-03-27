79680f696cc4d9154b521da0bfd06d9ee2b97c87
JhonHM01 <156867180+JhonHM01@users.noreply.github.com>
2024-03-26 22:26:29 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return ('huamani molina')
 
 if __name__ == "__main__":
     app.run(debug=True)
